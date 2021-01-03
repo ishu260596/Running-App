7d286fe3e6c37ebffc07a43eb3631f319b1a5d53
Ishwar Kumar <kumarishwar623@gmail.com>
2021-01-03 12:22:23 +0530

    My Runnung App Master

---
+++ RunningApp-master/app/src/main/java/com/androiddevs/runningapp/SplashScreenActivity.kt
@@ -0,0 +1,26 @@
+package com.androiddevs.runningapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.androiddevs.runningapp.ui.MainActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    lateinit var handler: Handler
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        handler= Handler()
+        handler.postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        }, 3000)
+
+        }
+    }
